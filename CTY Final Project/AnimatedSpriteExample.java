5ab3533f84c89ebe8ac00cf2ca7c5e4a42126327
RLKC8 <110426159+RLKC8@users.noreply.github.com>
2022-08-05 10:14:56 -0700

    Add files via upload

---
+++ CTY Final Project/AnimatedSpriteExample.java
@@ -0,0 +1,470 @@
+/* Basic example of how to do animation.
+
+   Make sure to call panel.mainLoop() in your main method!
+ */
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener; 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AnimatedSpriteExample extends JPanel implements KeyListener
+{
+  Scanner input = new Scanner(System.in);
+  int maxSpeed = 5;
+  boolean foot1 = true;
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+  boolean hasBasementKey=false;
+  boolean inBasement=true;
+  boolean inHouse=false;
+  String temp="";
+  public static final int FPS = 24;
+  public static final int MSPF = 1000/FPS;
+  int nx;
+  int ny;
+  BufferedImage[] images = new BufferedImage[12];
+  
+  BufferedImage background;
+  
+  BufferedImage basementBackground1;
+  
+  BufferedImage houseBackground1;
+  
+  BufferedImage BasementKey;
+  
+  EntityAnimation player;
+  
+  BufferedImage table;
+  
+  BufferedImage trapdoorOpen;
+  int area;
+  public static final Color Floor = new Color(127, 67, 3);
+  
+  ArrayList<Collisable>obstacles = new ArrayList<Collisable>();
+  
+  Collisable stairs = new Collisable(-315,-250, 20,20);
+  
+  Collisable stairs2 = new Collisable(-225,-240, 50, 15);
+
+  public AnimatedSpriteExample()
+  {
+    images[0] = readImage("Sprite Images/Left1.gif");
+    images[1] = readImage("Sprite Images/Left2.gif");
+    images[2] = readImage("Sprite Images/Left4.gif");
+    
+    images[3] = readImage("Sprite Images/Right1.gif");
+    images[4] = readImage("Sprite Images/Right2.gif");
+    images[5] = readImage("Sprite Images/Right4.gif");
+    
+    images[6] = readImage("Sprite Images/Forward1.gif");
+    images[7] = readImage("Sprite Images/Forward2.gif");
+    images[8] = readImage("Sprite Images/Forward4.gif");
+    
+    images[9] = readImage("Sprite Images/Backward1.gif");
+    images[10] = readImage("Sprite Images/Backward2.gif");
+    images[11] = readImage("Sprite Images/Backward4.gif");
+    
+    table = readImage("Sprite Images/Table(1).gif");
+    
+    BasementKey=readImage("Sprite Images/BasementKey.gif");
+    
+    basementBackground1 = readImage("Sprite Images/BasementBackground(1).gif");
+    
+    trapdoorOpen = readImage("Sprite Images/TrapdoorOpen.gif");
+    
+    nx = 300;
+    ny = 300;
+    player = new EntityAnimation(-300, 90, images);
+    background = basementBackground1;
+      addKeyListener(this);
+      setFocusable(true);
+     player.height = images[0].getHeight();
+     player.width = images[0].getWidth();
+     transition(0);
+     
+  }
+  
+  public void transition(int toArea)
+  {
+    obstacles = new ArrayList<Collisable>();
+    area = toArea;
+    if(area == 0)
+    {
+      player.x += 50;
+      background = basementBackground1;
+      obstacles.add(new Collisable(-300, -100, table));  
+      obstacles.get(0).height -= 60;
+      obstacles.get(0).width -=10;
+      
+      player.topBound = -270;
+      player.bottomBound = 110;
+      player.leftBound = -315;
+      player.rightBound = 130;
+      
+      //Stairiers
+      obstacles.add(new Collisable(-315, -150, 30 ,25)); //x, y, width, height
+      obstacles.add(new Collisable(-270, -130, 30, 15));
+      obstacles.add(new Collisable(-230, -110, 30, 5));
+      obstacles.add(new Collisable(-200, -90, 30, 1)); 
+    }
+    if(area == 1)
+    {
+      player.x -= 50; 
+      background = trapdoorOpen;
+      player.topBound = -900;
+      player.rightBound = -25;
+      player.leftBound = -860;
+      player.bottomBound = -50;
+      // Stair boundaries
+      obstacles.add(new Collisable(-230, -260, 50, 0));
+     
+      
+    }
+  }
+  /* Update the image and redraw the screen */
+  public void mainLoop()
+  {
+    long nextTick = System.currentTimeMillis();
+    long curTime;
+    
+ 
+    while(true)
+    {
+    if(!(player.middleAnimation())) 
+    {
+       if(rightPressed)
+      {
+        triggerRightWalk();
+      }
+     else if(leftPressed)
+      {
+       triggerLeftWalk();
+       
+      }
+     else if(upPressed)
+      {
+       triggerForwardWalk();
+
+      }
+     else if(downPressed)
+      {
+       triggerBackwardWalk();
+      }
+    }
+    if(rightPressed)
+      {
+        player.moveX(maxSpeed);
+        if(player.x > player.rightBound) {
+        player.moveX(-maxSpeed);
+        }
+        for(int i = 0; i < obstacles.size();  i++)
+        {
+          if(player.intersects(obstacles.get(i)))
+          {
+          player.moveX(-maxSpeed);
+          }
+        }
+      }
+      else if(leftPressed)
+      {
+        player.moveX(-maxSpeed);
+        if(player.x < player.leftBound) {
+        player.moveX(maxSpeed);
+        }
+         if(background == basementBackground1)
+        {
+          for(int i = 0; i < obstacles.size();  i++)
+          {
+            if(player.intersects(obstacles.get(i)))
+            {
+            player.moveX(maxSpeed);
+            }
+          }
+        }
+      }
+      else if(upPressed)
+      {
+        player.moveY(-maxSpeed);
+        if(player.y < player.topBound) {
+        player.moveY(maxSpeed);
+        }
+        if(background == basementBackground1)
+        {
+         for(int i = 0; i < obstacles.size();  i++)
+        {
+          if(player.intersects(obstacles.get(i)))
+          {
+          player.moveY(maxSpeed);
+          }
+        }
+        }
+      }
+      else if(downPressed)
+      {
+        player.moveY(maxSpeed);
+        if(player.y > player.bottomBound) {
+        player.moveY(-maxSpeed);
+        }
+         
+          for(int i = 0; i < obstacles.size();  i++)
+          {
+            if(player.intersects(obstacles.get(i)))
+            {
+            player.moveY(-maxSpeed);
+            }
+          }
+      }
+
+/*    else
+    {
+      if(!(rightPressed || leftPressed || upPressed || downPressed))
+      {
+        player.clearAnimation();
+      } 
+    } */
+      
+
+      // change game state
+      player.step();
+      
+      checkConditions();
+     
+      
+      // wait some time
+      curTime = System.currentTimeMillis();
+      if(nextTick > curTime)
+      {
+        try
+        {
+          Thread.sleep(nextTick - curTime);
+        }
+        catch(Exception e){e.printStackTrace();}
+      }
+      
+      nextTick += MSPF;
+      
+      // redraw screen
+      while ((player.x>=-315)&&(player.x<=-185) && (player.y==-45)&&inBasement){
+         if (!hasBasementKey){
+            System.out.println("You see a key lying on the table. Do you grab it?");
+            temp = input.nextLine().toLowerCase();
+            if (temp.startsWith("y")){
+               System.out.println("You grab the key.");
+               hasBasementKey=true;
+               player.moveY(5);
+            }
+            else{
+               System.out.println("You walk away.");
+               player.moveY(5);
+            }
+
+         }
+       }
+
+      while ((player.x>=-315)&&(player.x<=-295) && (player.y<=-210)&&(player.y>=-270)&&inBasement){
+         if (hasBasementKey){
+            System.out.println("You use the key to open a trapdoor locking you in.");
+            hasBasementKey=false;
+            inBasement=false;
+            inHouse=true;
+            background=houseBackground1;
+         }
+         else{
+            System.out.println("You try to push the trapdoor locking you in, but it won't budge. You notice a small keyhole.");
+            player.moveX(25);
+         }
+      }
+
+      repaint();
+    }
+  } 
+ 
+  
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    g.setColor(Color.BLACK);
+    g.fillRect(0,0,1800,1800);
+    if(area==1){
+      g.setColor(Floor);
+      g.fillRect(-player.x -550,-player.y -550, 900, 900);
+      }
+    g.drawImage(background, -player.x,-player.y, null);
+    for(int i = 0; i < obstacles.size();  i++)
+    {
+     obstacles.get(i).drawTo(g, -player.x + nx, -player.y + ny); //-315,-15
+    } 
+    player.drawTo(g,nx,ny);
+    g.setColor(Color.WHITE);
+    g.drawString(player.x +", "+ player.y,1030, 20);
+    if (!hasBasementKey&&inBasement){
+      g.drawImage(BasementKey, 75-player.x, 225-player.y, null);
+    }
+  }
+  
+  public void triggerLeftWalk()
+  {
+    if(foot1 == true) {
+    player.startAnimationSequence(new int[][]{
+        {1,7},
+        {0,7},
+     }
+   );
+   }
+     else{
+     player.startAnimationSequence(new int[][]{
+        {2,7},
+        {0,7},
+       }
+    );
+    }
+    foot1 = !foot1;
+    player.setIdleFrame(0);
+  }
+  
+  public void triggerRightWalk()
+  {
+    if(foot1 == true) 
+    {
+    player.startAnimationSequence(new int[][]{
+        {4,7},
+        {3,7},
+      }
+    );
+  }
+    else
+    {
+    player.startAnimationSequence(new int[][]{
+        {5,7},
+        {3,7},
+    }
+   ); 
+    }
+    foot1 = !foot1;
+    player.setIdleFrame(3);
+  }
+  public void triggerForwardWalk()
+  {
+  if(foot1 == true) 
+  {
+    player.startAnimationSequence(new int[][]{
+        {7,7},
+        {6,7},
+      }
+    );
+  }
+  else
+  {
+  player.startAnimationSequence(new int[][] {
+   {8,7},
+   {6,7},
+  }
+  );
+  }
+    foot1 = !foot1;
+    player.setIdleFrame(6);
+  }
+  public void triggerBackwardWalk()
+  {
+  if(foot1 == true)
+  {
+    player.startAnimationSequence(new int[][]{
+        {10,7},
+        {9,7},
+        
+      }
+    );
+  }
+  else
+  {
+   player.startAnimationSequence(new int[][]{
+        {11,7},
+        {9,7},
+     }
+   );
+  }
+    foot1 = !foot1;
+    player.setIdleFrame(9);
+  }
+    public static BufferedImage readImage(String infile)
+  {
+    try
+    {
+      BufferedImage ret = ImageIO.read(new File(infile));
+      return ret;
+    }
+    catch(Exception e){System.out.println(e.getMessage()+ infile); return null;}
+  }
+    public void keyPressed(KeyEvent e)
+  {
+    int code = e.getKeyCode();
+    
+    if(code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D)
+    {
+      rightPressed = true;
+    }
+    else if(code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A)
+    {
+      leftPressed = true;
+    }
+    else if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W)
+    {
+      upPressed = true;
+    }
+    else if(code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S)
+    {
+      downPressed = true;
+    }
+  }
+  
+  public void keyReleased(KeyEvent e)
+  {
+    int code = e.getKeyCode();
+    
+    if(code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D)
+    {
+      rightPressed = false;
+    }
+    else if(code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A)
+    {
+      leftPressed = false;
+    }
+    else if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W)
+    {
+      upPressed = false;
+    }
+    else if(code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S)
+    {
+      downPressed = false;
+    }
+  }
+  public void keyTyped(KeyEvent e)
+  {
+  }
+  public void checkConditions()
+  {
+    if(player.intersects(stairs))
+    {   
+      if(area == 0)
+      {
+      transition(1);
+      }
+    }
+    if(player.intersects(stairs2))
+    {
+      if(area == 1)
+      {
+      transition(0);
+      }
+    }
+  }
+
+}
--- CTY Final Project/FinalProject.java
+++ CTY Final Project/FinalProject.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
+import javax.swing.JFrame;
 public class FinalProject{
    public static void main(String args[]){
       Scanner input = new Scanner(System.in);
       Random rng = new Random();
-      Player player=new Player();
+      AnimatedSpriteExample panel = new AnimatedSpriteExample();
+      Player player=new Player(panel.player);
       int temp;
       int nextInt;
-      boolean canProgress=true;
       boolean hasBasementKey=false;
       String nextLine;
       System.out.println("Please type out your character's name.");
-      player.name=input.nextLine();
+      player.name=input.nextLine().toLowerCase();
       System.out.println("Ok, "+player.name+", let's start by assigning your skill points.");
-      System.out.println("You have 20 points to distribute among strength, dexterity, constitution, wisdom, and magic.\nYou start with a value of 1 in each category.");
+      try{Thread.sleep(1000);}catch(InterruptedException e){}
+      System.out.println("You have 10 points to distribute among strength, dexterity, constitution, wisdom, and magic.");
+      try{Thread.sleep(1000);}catch(InterruptedException e){}
+      System.out.println("You start with a value of 3 in each category.");
+      try{Thread.sleep(1000);}catch(InterruptedException e){}
       System.out.println("Let's start with strength. How many points would you like to alot to strength?");
       temp=0;
       while (temp==0){
          nextInt = input.nextInt();
-         input.nextLine();
+         input.nextLine().toLowerCase();
          if (nextInt<=player.skillPoints & nextInt>=0){
             System.out.println("Are you sure?");
-            nextLine = input.nextLine();
+            nextLine = input.nextLine().toLowerCase();
             if (nextLine.startsWith("y")){
                player.strength+=nextInt;
                player.skillPoints-=nextInt;
                System.out.println("You now have a strength value of "+player.strength+". You have "+player.skillPoints+" skill points left.");
                temp++;
+               try{Thread.sleep(1000);}catch(InterruptedException e){}
                System.out.println("Next, dexterity, or speed. How many points would you like to alot to dexterity?");
             }
             else{
@@ -35,20 +41,25 @@ public class FinalProject{
             }
          }
          else{
-            System.out.println("You don't have enough points, try again! \nYou have "+player.skillPoints+" skill points left. \nHow many points would you like to alot to strength?");
+            System.out.println("You don't have enough points, try again!");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("You have "+player.skillPoints+" skill points left.");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("How many points would you like to alot to strength?");
          }
       }
       while (temp==1){
          nextInt = input.nextInt();
-         input.nextLine();
+         input.nextLine().toLowerCase();
          if (nextInt<=player.skillPoints & nextInt>=0){
             System.out.println("Are you sure?");
-            nextLine = input.nextLine();
+            nextLine = input.nextLine().toLowerCase();
             if (nextLine.startsWith("y")){
                player.dexterity+=nextInt;
                player.skillPoints-=nextInt;
                System.out.println("You now have a dexterity value of "+player.dexterity+". You have "+player.skillPoints+" skill points left.");
                temp++;
+               try{Thread.sleep(1000);}catch(InterruptedException e){}
                System.out.println("Next, constitution, or defense. How many points would you like to alot to constitution?");
             }
             else{
@@ -56,20 +67,25 @@ public class FinalProject{
             }
          }
          else{
-            System.out.println("You don't have enough points, try again! \nYou have "+player.skillPoints+" skill points left. \nHow many points would you like to alot to dexterity?");
+            System.out.println("You don't have enough points, try again!");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("You have "+player.skillPoints+" skill points left.");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("How many points would you like to alot to dexterity?");         
          }
       }
       while (temp==2){
          nextInt = input.nextInt();
-         input.nextLine();
+         input.nextLine().toLowerCase();
          if (nextInt<=player.skillPoints & nextInt>=0){
             System.out.println("Are you sure?");
-            nextLine = input.nextLine();
+            nextLine = input.nextLine().toLowerCase();
             if (nextLine.startsWith("y")){
                player.constitution+=nextInt;
                player.skillPoints-=nextInt;
                System.out.println("You now have a constitution value of "+player.constitution+". You have "+player.skillPoints+" skill points left.");
                temp++;
+               try{Thread.sleep(1000);}catch(InterruptedException e){}
                System.out.println("Next, wisdom. How many points would you like to alot to wisdom?");
             }
             else{
@@ -77,22 +93,27 @@ public class FinalProject{
             }
          }
          else{
-            System.out.println("You don't have enough points, try again! \nYou have "+player.skillPoints+" skill points left. \nHow many points would you like to alot to dexterity?");
+            System.out.println("You don't have enough points, try again!");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("You have "+player.skillPoints+" skill points left.");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("How many points would you like to alot to constitution?");         
          }
       }
       while (temp==3){
          nextInt = input.nextInt();
-         input.nextLine();
+         input.nextLine().toLowerCase();
          if (nextInt<=player.skillPoints & nextInt>=0){
             System.out.println("Are you sure?");
-            nextLine = input.nextLine();
+            nextLine = input.nextLine().toLowerCase();
             if (nextLine.startsWith("y")){
-               player.wisdom+=nextInt;
-               player.skillPoints-=nextInt;
+               player.wisdom+=nextInt;               player.skillPoints-=nextInt;
                System.out.println("You now have a wisdom value of "+player.wisdom+". You have "+player.skillPoints+" skill points left.");
                temp++;
-               System.out.println("Lastly, magic. The rest of your skill point will be aloted to magic, so "+player.skillPoints+" skill points will go to magic. You have a magic value of "+player.magic+".");
+               try{Thread.sleep(1000);}catch(InterruptedException e){}
                player.magic+=player.skillPoints;
+               System.out.println("Lastly, magic. The rest of your skill point will be aloted to magic, so "+player.skillPoints+" skill points will go to magic. You have a magic value of "+player.magic+".");
+               
                player.skillPoints=0;
             }
             else{
@@ -100,18 +121,38 @@ public class FinalProject{
             }
          }
          else{
-            System.out.println("You don't have enough points, try again! \nYou have "+player.skillPoints+" skill points left. \nHow many points would you like to alot to wisdom?");
+            System.out.println("You don't have enough points, try again!");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("You have "+player.skillPoints+" skill points left.");
+            try{Thread.sleep(1000);}catch(InterruptedException e){}
+            System.out.println("How many points would you like to alot to wisdom?");         
          }
       }
-      System.out.println("You wake up in a dimly lit basement. There's a shiny object on a nearby table. Try using WASD or your arrow keys to move around.");
-      while ((player.x>-185)&&(player.x<-175) && (player.y<-205)&&(player.y>-310) && canProgress==true){
-         if (hasBasementKey=true){
-            System.out.println("You use the key to open a trapdoor locking you in.");
-            canProgress=false;
-         }
-         else{
-            System.out.println("You try to push the trapdoor locking you in, but it won't budge. You notice a small keyhole.");
-         }
-      }
+      // Initialize the window
+       JFrame frame = new JFrame();
+   
+       // Set the width and height of the window in pixels
+       frame.setSize(1024,1024);
+       
+       // Make the program end when the window is closed
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       // Initialize the drawing canvas (replace BGPExample with the name of any
+       // class that extends JPanel)
+   
+       
+      
+       // Add the drawing canvas to the window
+       frame.add(panel);
+       
+       // Show the window when we are done with all of our initialization
+       frame.setVisible(true);
+       
+       System.out.println("You wake up in a dimly lit basement. There's a shiny object on a nearby table. Try using WASD or your arrow keys to move around.");
+
+       panel.mainLoop();
+            
+      
+       
    }
 }
